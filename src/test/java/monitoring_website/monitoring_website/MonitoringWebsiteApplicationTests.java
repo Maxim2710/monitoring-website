3f0e230827bcb8f06080cf5fab7ebac6db6a7a4a
PrimakMV <pm2710@mail.ru>
2024-11-09 00:40:01 +0300

    Added a README file for navigating the application

---
+++ src/test/java/monitoring_website/monitoring_website/MonitoringWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package monitoring_website.monitoring_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
