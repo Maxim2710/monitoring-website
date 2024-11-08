bf84f3ef406539cf11d0ae814be274a27099bbfc
PrimakMV <pm2710@mail.ru>
2024-11-09 00:35:11 +0300

    The initial structure of the project has been added

---
+++ src/main/java/monitoringWebsite/monitoringWebsite/MonitoringWebsiteApplication.java
@@ -0,0 +1,13 @@
+package monitoringWebsite.monitoringWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringWebsiteApplication.class, args);
+	}
+
+}
